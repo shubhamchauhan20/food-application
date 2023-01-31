d545d95f7e5f5e4edabd4ea84e70db60e74608a2
shubhamchauhan20 <shubhamchauhan55t@gmail.com>
2023-01-31 21:35:37 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='p-2 my-0 text-lg m-2 bg-cyan-500 '>
       <h1 className='font-bold'>Created by</h1>
-      <h1 className='text-cyan-800'>Shubham Chauhanⓒ</h1>
+      <h1 className='text-blue-600'>Shubham Chauhanⓒ</h1>
     </div>
   )
 }
